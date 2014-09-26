a64b47662e09bbc2a50c60eabb94712719f35e88
Michael Louie Loria <michaellouieloria@gmail.com>
2014-09-26 20:21:04 +0800

    login

--- cms/views.py
+++ cms/views.py
@@ -2,7 +2,6 @@ from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponseRedirect
-from django.contrib import auth
 from django.template import RequestContext
 from django.contrib.auth.forms import UserCreationForm
 from django.core.context_processors import csrf
@@ -53,26 +52,6 @@ def contact(request):
 def thanks(request):
     return render(request, 'thanks.html')
 
-def login(request):
-    message = None
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    auth.login(request, user)
-                    return HttpResponseRedirect('/accounts/loggedin')
-                else:
-                    message = "Your account is inactive"
-            else:
-                message = "Invalid username and/or password, please reenter"
-    else:
-        form = LoginForm()
-    return render_to_response('registration/login.html', {'message': message, 'form': form},
-                             context_instance=RequestContext(request))
 
 def loggedin(request):
     return render(request, 'registration/loggedin.html',
